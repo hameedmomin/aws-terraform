0812bea53c66d53799e49a59d41bc7571bdf11ed
USER <hameed.momin@gmail.com>
2022-09-14 18:47:42 +0530

    new

--- terraform block/vpc.tf
+++ terraform block/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "firstvpc" {
 }
 resource "aws_subnet" "publicsubnet" {
   vpc_id     = aws_vpc.firstvpc.id
-  cidr_block = "10.1.0.0/24"
+  cidr_block = "10.0.0.0/24"
 
   tags = {
     Name = "publicsubnet"
@@ -18,7 +18,7 @@ resource "aws_subnet" "publicsubnet" {
 }
 resource "aws_subnet" "privatesubnet" {
   vpc_id     = aws_vpc.firstvpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
 
   tags = {
     Name = "privatesubnet"

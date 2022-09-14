0278d6d3aef01727ea5f39bf67a0260d4baf4b39
USER <hameed.momin@gmail.com>
2022-09-14 13:16:18 +0530

    new

--- terraform block/ec2.tf
+++ terraform block/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_spot_instance_request" "myinstances" {
   ami           = "ami-0bb6af715826253bf"
   spot_price    = "0.03"
-  count         =  3
   instance_type = "t2.micro"
+user_data     = file("${path.module}/"
 
-  tags = {
-    Name = "CheapWorker"
+  tags  {
+    Name = "ec2-1"
   }
+
 }

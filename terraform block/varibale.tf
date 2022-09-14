8c8454b76ff892420a6ebee66eee1229d40d8347
USER <hameed.momin@gmail.com>
2022-09-14 18:22:02 +0530

    new

--- terraform block/varibale.tf
+++ terraform block/varibale.tf
@@ -8,11 +8,11 @@ variable "instances_type" {
   default = "t2.micro"
   type = string
 }
-variable "CIRD_BLOCK" {
+/*variable "CIRD_BLOCK" {
   default = "10.0.0.0/16"
   type    = number
 
-}
+}*/
 variable "vpcname" {
   default = "myfirstvpc"
   type    = string

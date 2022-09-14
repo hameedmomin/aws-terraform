c2dd0038944b181a2057e297194576be08276cb5
USER <hameed.momin@gmail.com>
2022-09-14 10:56:00 +0530

    new

---
+++ terraform block/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~> 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "aws-terraform/hameed"
+    key = "myterraform/tf.state"
+    region = "us-east-1"
+    dynamo_db = "mytable"
+  }
+}
+

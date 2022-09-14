12da37074e1e83a9a2bf948704e27e1c0f96d9f5
USER <hameed.momin@gmail.com>
2022-09-14 10:36:31 +0530

    new

---
+++ LB/alb.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+      }
+    }
+
+
+
+ backend "s3" {
+        bucket = "aws-terraform/hameed"
+        key = "myterraform/tf.state"
+        region = "us-east-1"
+        dynamo_db = "mytable"
+  }
+}

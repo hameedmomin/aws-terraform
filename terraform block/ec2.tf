17b5ee02c53566415ff612dce8e96c54966c0712
USER <hameed.momin@gmail.com>
2022-09-15 10:23:12 +0530

    new

--- terraform block/ec2.tf
+++ terraform block/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "myinstances" {
   ami           = "ami-0bb6af715826253bf"
   spot_price    = "0.03"
-  count         = 2
+  count         = length(var.components)
   instance_type = length(var.instances_type-list)
   user_data     = file("${path.module}/httpdserver.sh")
 

0ada3d431589f74507c36e705b42563dd8a62800
USER <hameed.momin@gmail.com>
2022-09-16 10:27:16 +0530

    new

--- terraform block/ec2.tf
+++ terraform block/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "myinstances" {
   ami           = "ami-0bb6af715826253bf"
   spot_price    = "0.035"
-  for_each      = toset(data.aws_availability_zones.all.names)
+  for_each      = toset(data.aws_ec2_instance_type_offerings.types.instance_types)
   instance_type = var.instances_type-map["sabbi"]
   user_data     = file("${path.module}/httpdserver.sh")
   availability_zone = each.key
@@ -11,17 +11,17 @@ resource "aws_spot_instance_request" "myinstances" {
   }
 }
 
-data "aws_availability_zones" "all" {
+/*data "aws_availability_zones" "all" {
   filter {
     name   = "opt-in-status"
     values = ["opt-in-not-required"]
   }
-}
+}*/
 data "aws_ec2_instance_type_offerings" "types" {
   for_each = toset(["us-eat-1a", "us-eat-1b", "us-eat-1e"])
   filter {
     name   = "instance-type"
-    values = ["t2.micro", "t3.micro"]
+    values = ["t3.micro"]
   }
 
   filter {

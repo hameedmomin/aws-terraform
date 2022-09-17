b518611572697f91cadd7f605ec0ba14e89f5e81
USER <hameed.momin@gmail.com>
2022-09-17 12:25:37 +0530

    new

--- terraform block/data.ami.tf
+++ terraform block/data.ami.tf
@@ -1,6 +1,6 @@
-data "aws_ami" "centos7" {
+data "aws_ami" "centos8" {
   most_recent      = true
-  name_regex       = "^Centos-7-DevOps-Practice"
+  name_regex       = "^Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 
 }
--- terraform block/ec2-more.tf
+++ terraform block/ec2-more.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "spot" {
-  ami                         = data.aws_ami.centos7
+  ami                         = data.aws_ami.centos8
   for_each                    = toset(keys({ for getting, avail in data.aws_ec2_instance_type_offerings.my_types : getting => avail.instance_types if length(avail.instance_types) !=0}))
   spot_price                  = "0.035"
   instance_type               = var.instance_type

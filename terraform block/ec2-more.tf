fbe9a21fefce057033b6e0b54d368267a216b65b
USER <hameed.momin@gmail.com>
2022-09-17 12:28:52 +0530

    new

--- terraform block/ec2-more.tf
+++ terraform block/ec2-more.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_spot_instance_request" "spot" {
   ami                         = toset(data.aws_ami.centos8)
   for_each                    = toset(keys({ for getting, avail in data.aws_ec2_instance_type_offerings.my_types : getting => avail.instance_types if length(avail.instance_types) !=0}))
@@ -8,4 +9,4 @@ resource "aws_spot_instance_request" "spot" {
   tags = {
     Name                      = "demo-${each.key}"
   }
-}
+}*/

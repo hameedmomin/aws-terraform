538f6c7733c73fdbd2d66a88bebbb1142ce9d30b
USER <hameed.momin@gmail.com>
2022-09-18 21:49:07 +0530

    new

--- backend/state.tf
+++ backend/state.tf
@@ -1,8 +1,11 @@
+/*
 terraform {
   backend "s3" {
     bucket            = "hameed"
     dynamodb_table    = "my-project"
     key               = "terraform/practise/terraform.tfstate"
     region            = "us-east-1"
+
   }
 }
+*/

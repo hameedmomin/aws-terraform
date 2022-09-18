43c94142f115aff5186aed9ae3822ce0ad627dd7
USER <hameed.momin@gmail.com>
2022-09-18 20:20:06 +0530

    new

--- backend/s3.tf
+++ backend/s3.tf
@@ -1,4 +1,15 @@
-resource "aws_s3_bucket_object" "mybuckett" {
+/*resource "aws_s3_bucket_object" "mybuckett" {
   bucket                  = "hameed"
   key                     = "my-storage"
+}*/
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "hameed"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+output "bukcetname" {
+  value = aws_s3_bucket.mybucket
 }

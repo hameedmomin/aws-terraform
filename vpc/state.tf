c20c57460ad1aeaee3a69079a214849d05c3c46c
USER <hameed.momin@gmail.com>
2022-09-19 10:49:30 +0530

    new

---
+++ vpc/state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket            = "hameed"
+    dynamodb_table    = "my-project"
+    key               = "terraform/practise/terraform.tfstate"
+    region            = "us-east-1"
+
+  }
+}

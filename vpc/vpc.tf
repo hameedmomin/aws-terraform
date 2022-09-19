f215aedcc49caa14c68614cf19ad9225f13167fc
USER <hameed.momin@gmail.com>
2022-09-19 13:05:30 +0530

    new

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   name                                          = "my_vpc"
   cidr                                          = "20.0.0.0/16"
-  azs                                           = [ "us-eat-1a", "us-east-1b"]
+  azs                                           = [ "us-east-1a", "us-east-1b"]
   private_subnets                               = ["20.0.1.0/24", "20.0.2.0/24"]
   public_subnets                                = ["20.0.11.0/24", "20.0.12.0/24"]
 

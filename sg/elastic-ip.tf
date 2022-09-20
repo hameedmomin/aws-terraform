3dfe2b3c395c74900f664ecbad5dce888d2174c0
USER <hameed.momin@gmail.com>
2022-09-20 16:12:54 +0530

    new

--- sg/elastic-ip.tf
+++ sg/elastic-ip.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "bastion_eip" {
-  instance                 = module.ec2_bastion.id[0]
+  instance                 = module.ec2_bastion.id
   vpc                      = true
   depends_on               = [ module.vpc, module.ec2_bastion ]
   tags                     = local.common_tags

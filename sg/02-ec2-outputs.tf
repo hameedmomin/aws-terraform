cd49db246c5761cbb87e4d578b49a600e2cbba9c
USER <hameed.momin@gmail.com>
2022-09-20 15:46:09 +0530

    new

--- sg/02-ec2-outputs.tf
+++ sg/02-ec2-outputs.tf
@@ -10,7 +10,7 @@ output "ec2_private_id" {
   description                     = "The ID of the instance"
   value                           = module.ec2_private.id
 }
-output "ec2_private_ip" {
+output "ec2_private-ip" {
   description                     = "The Private IP address of the instance"
   value                           = module.ec2_private.private_ip
 }

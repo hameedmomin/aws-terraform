8aaaaa2cc2d909a4d76798aa45dd59a715168dc4
USER <hameed.momin@gmail.com>
2022-09-20 11:36:28 +0530

    new

--- sg/sg-privatesg.tf
+++ sg/sg-privatesg.tf
@@ -5,8 +5,8 @@ module "private_sg" {
   name                       = "private-sg"
   vpc_id                     = module.vpc.vpc_id
   ingress_rules              = ["ssh-tcp", "http-80-tcp"]
-  ingress_cidr_block         = [module.vpc.vpc_cidr_block]
-  egress_rule                = ["all-all"]
+  ingress_cidr_blocks        = [module.vpc.vpc_cidr_block]
+  egress_rules               = ["all-all"]
   tags                       = local.common_tags
 
 }

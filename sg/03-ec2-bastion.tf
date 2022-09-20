8b4160a746eea03e7232a4b6c92c51b26800558d
USER <hameed.momin@gmail.com>
2022-09-20 11:24:31 +0530

    new

--- sg/03-ec2-bastion.tf
+++ sg/03-ec2-bastion.tf
@@ -1,12 +1,12 @@
 module "ec2_bastion" {
-  source                = "terraform-aws-modules/ec2-instance/aws"
-  version               = "4.1.4"
-  name                  = "${var.environment}-BastionHost"
-  ami                   = data.aws_ami.centos8.id
-  instances_type        = var.instance_type
-  subnet_id             = module.vpc.public_subnets[0]
-  vpc_security_group_id = [module.public_bastion_sg.bastion_security_group_id]
-  tags                  = local.common_tags
+  source                       = "terraform-aws-modules/ec2-instance/aws"
+  version                      = "4.1.4"
+  name                         = "${var.environment}-BastionHost"
+  ami                          = data.aws_ami.centos8.id
+  instance_type               = var.instance_type
+  subnet_id                    = module.vpc.public_subnets[0]
+  vpc_security_group_ids       = [module.public_bastion_sg.bastion_security_group_id]
+  tags                         = local.common_tags
 }
 
 

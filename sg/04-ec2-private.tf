851a3d94963f7669f1936285795e1e9df0026b9c
USER <hameed.momin@gmail.com>
2022-09-20 11:27:26 +0530

    new

--- sg/04-ec2-private.tf
+++ sg/04-ec2-private.tf
@@ -4,11 +4,12 @@ module "ec2_private" {
   depends_on                  = [module.vpc]
   name                        = "${var.environment}-Private"
   ami                         = data.aws_ami.centos8.id
-  instances_count             = var.private_instance_count
-  instances_type              = var.instance_type
+  instance_count             = var.private_instance_count
+  instance_type               = var.instance_type
 #  subnet_id                  = module.vpc.private_subnets[0]
-  subnet_ids                  = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
-  vpc_security_group_id       = [module.public_bastion_sg.bastion_security_group_id]
+  subnet_ids                  = [module.vpc.private_subnets[0],
+                                 module.vpc.private_subnets[1]]
+  vpc_security_group_ids      = [module.public_bastion_sg.bastion_security_group_id]
   tags                        = local.common_tags
   user_data                   = file("${path.module}/apache.sh")
 

e0f3149a3253213e429ce14992db70e037eb4840
USER <hameed.momin@gmail.com>
2022-09-20 11:35:35 +0530

    new

--- sg/sg-bastion.tf
+++ sg/sg-bastion.tf
@@ -5,8 +5,8 @@ module "public_bastion_sg" {
   name                       = "public-bastion-sg"
   vpc_id                     = module.vpc.vpc_id
   ingress_rules              = ["ssh-tcp"]
-  ingress_cidr_block         = ["0.0.0.0/16"]
-  egress_rule                = ["all-all"]
+  ingress_cidr_blocks        = ["0.0.0.0/16"]
+  egress_rules               = ["all-all"]
   tags                       = local.common_tags
 
 }

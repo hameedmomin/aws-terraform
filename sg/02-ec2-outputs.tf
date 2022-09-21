cf7e686c210126cce9782b964e6526e0fd74a6b2
USER <hameed.momin@gmail.com>
2022-09-21 11:35:15 +0530

    new

--- sg/02-ec2-outputs.tf
+++ sg/02-ec2-outputs.tf
@@ -6,9 +6,9 @@ output "ec2_bastion_public_ip" {
   description                     = "The Public IP address of the instance"
   value                           = module.ec2_bastion.public_ip
 }
-output "ec2_private_id" {
+output "ec2_private-id" {
   description                     = "The ID of the instance"
-  value                           = module.ec2_private.id
+  value                           = module.ec2_private.ids
 }
 output "ec2_private-ip" {
   description                     = "The Private IP address of the instance"

c3e54ee71ccce7e2d1960665f260cd7b3e29a9c7
USER <hameed.momin@gmail.com>
2022-09-21 20:00:35 +0530

    new

--- ELB/03-classicLB.tf
+++ ELB/03-classicLB.tf
@@ -41,7 +41,10 @@ module "classicLB" {
 
   // ELB attachments
   number_of_instances            = var.private_instance_count
-  instances                      = [ module.ec2_private.id[0], module.ec2_private.id[1] ]
+  instances                      = [
+    module.ec2_private.id[0],
+    module.ec2_private.id[1]
+  ]
 
   tags                           = local.common_tags
 }

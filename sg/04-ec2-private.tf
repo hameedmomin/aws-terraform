3d23c6f1e694e56c3fb3950541c072ec844c1aa4
USER <hameed.momin@gmail.com>
2022-09-21 11:50:58 +0530

    new

--- sg/04-ec2-private.tf
+++ sg/04-ec2-private.tf
@@ -6,7 +6,7 @@ module "ec2_private" {
   ami                         = data.aws_ami.centos8.id
   instance_type               = var.instance_type
 #  subnet_id                  = module.vpc.private_subnets[0]
-  subnet_ids                   = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  subnet_id                   = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
   count                       = var.private_instance_count
   vpc_security_group_ids      = [module.public_bastion_sg.security_group_id]
   tags                        = local.common_tags

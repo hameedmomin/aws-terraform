6c190d73b98103a81d09b9e27e044db0acce7a98
USER <hameed.momin@gmail.com>
2022-09-28 18:43:00 +0530

    new

--- ALB_Basic/03-ALB.tf
+++ ALB_Basic/03-ALB.tf
@@ -58,11 +58,11 @@ module "alb-basic" {
       }
       targets  = {
         my_targets_pvt1     = {
-          target_id         = module.ec2_private.id[0]
+          target_id         = module.ec2_private.id
           port              = 80
       },
         my_targets_pvt2     = {
-          target_id         = module.ec2_private.id[1]
+          target_id         = module.ec2_private.id
           port              = 80
         }
       }

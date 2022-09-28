7f4b94bee07aa20fd518dc7cd0bd39937bfa8c19
USER <hameed.momin@gmail.com>
2022-09-28 20:30:19 +0530

    new

--- ALB_Basic/01-ec2-varibles.tf
+++ ALB_Basic/01-ec2-varibles.tf
@@ -2,7 +2,7 @@ variable "instance_type" {
   default = "t3.micro"
 }
 variable "private_instance_count" {
-  default   = 1
+#  default   = 1
   type      = number
   description = "AWS private instances count"
 }

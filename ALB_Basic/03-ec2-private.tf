1f82f25d33f404c2914664aed3c4922093d1fa66
USER <hameed.momin@gmail.com>
2022-09-28 20:41:06 +0530

    new

--- ALB_Basic/03-ec2-private.tf
+++ ALB_Basic/03-ec2-private.tf
@@ -7,7 +7,7 @@ module "ec2_private" {
   instance_type               = var.instance_type
   subnet_id                   = module.vpc.private_subnets[0]
 #  subnet_id                   = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]  ]
-  count                       =  length(var.private_instance_count[*])
+  count                       =  length(toset(var.private_instance_count[*]))
   vpc_security_group_ids      = [module.private_sg.security_group_id]
   tags                        = local.common_tags
   user_data                   = file("${path.module}/apache.sh")

96cf380e5b0fd614dce90c4d3c32437d68c80e2b
USER <hameed.momin@gmail.com>
2022-09-29 10:38:29 +0530

    new

--- ALB_Basic/03-ec2-private.tf
+++ ALB_Basic/03-ec2-private.tf
@@ -15,5 +15,17 @@ module "ec2_private" {
 }
 
 
+module "ec2_bastion" {
+  source                       = "terraform-aws-modules/ec2-instance/aws"
+  version                      = "4.1.4"
+  name                         = "${var.environment}-BastionHost"
+  ami                          = data.aws_ami.centos8.id
+  instance_type                = var.instance_type
+  subnet_id                    = module.vpc.public_subnets[0]
+  vpc_security_group_ids       = [module.public_bastion_sg.security_group_id]
+  tags                         = local.common_tags
+}
+
+
 
 
